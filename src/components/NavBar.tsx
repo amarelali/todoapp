8329f7d2b6103e950cbf72747a5b2a3f5e704abb
amarelali <amar.elali@isae.edu.lb>
2024-02-07 20:49:01 +0200

    fix types in modal

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure, Menu, Transition  } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [{ name: "My TODO", href: "#", current: true }];
@@ -11,7 +11,7 @@ function classNames(...classes: string[]) {
 export default function NavBar() {
   return (
     <Disclosure as="nav" className="bg-white-800">
-      {({ open }) => (
+      {({ open }:{open:boolean}) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
@@ -90,7 +90,7 @@ export default function NavBar() {
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
-                        {({ active }) => (
+                        {({ active }:{active:boolean}) => (
                           <a
                             href="#"
                             className={classNames(
